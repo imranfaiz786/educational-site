38b787243cee09fcb27b734e9686d5885a4151e5
imranfaiz786 <emranfaiz786@gmail.com>
2025-04-07 20:51:42 +0500

    Initial MERN project

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import slideRoutes from './routes/slideRoutes.js';
+import slideRoutes from '../routes/slideRoutes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
